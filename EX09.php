7510c99c9b6db9fc749f3a4b66ede4d8e9a41455
Kael Bezerra <74618065+Sim3Kael@users.noreply.github.com>
2023-05-10 19:24:07 -0300

    Add files via upload

---
+++ EX09.php
@@ -0,0 +1,26 @@
+<?php
+
+$caso = readline();
+
+$j = 0;
+
+while ($caso > $j) {
+    $saida = "";
+
+    $palavra = readline();
+
+    $palavra = str_split($palavra);
+
+    $palavra = array_reverse($palavra);
+
+    for($i = 0; $i < count($palavra); $i++) {
+
+        if(ctype_lower($palavra[$i])) {
+            $saida .= "$palavra[$i]";
+        }
+
+    }
+    
+    echo $saida . "\n";
+    $caso--;
+}
